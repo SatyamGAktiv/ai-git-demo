a89c01c185b27f53472f6a30e769a68c73ddb435
janvi-wibtec <janvi@wibtec.com>
2026-04-07 11:26:12 +0530

    [IMP] Added words concatenate to sentence.

--- greet.py
+++ greet.py
@@ -4,3 +4,11 @@ print(f"Hello, {name}!")
 print("Here's a star pattern for you:")
 for i in range(1, 6):
     print("*" * i)
+
+words = ["Hello", "this", "is", "a", "test"]
+
+sentence = ""
+for word in words:
+    sentence = sentence + word + " "
+
+print(sentence)
