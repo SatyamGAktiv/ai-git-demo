2ccbb3284e90f9a53c88c08c18968c7f4a4fafae
SatyamGAktiv <satyamg.aktiv@gmail.com>
2026-04-07 15:42:54 +0530

    Optimize greeting output and remove extra print

--- greet.py
+++ greet.py
@@ -1,11 +1,5 @@
 name = input("Please enter your name: ").strip()
 print(f"Hello, {name}!")
 
-words = ["Hello", "this", "is", "a", "test"]
-
-# Build the sentence without adding a trailing space.
-sentence = " ".join(words)
-
-print(sentence)
-
-print("this is changes")
+WORDS = ("Hello", "this", "is", "a", "test")
+print(" ".join(WORDS))
